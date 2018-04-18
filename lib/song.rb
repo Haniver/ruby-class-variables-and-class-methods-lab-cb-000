67a8a50318780eb7ae8c86fdbed3f18de2c4c808
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-04-18 19:47:08 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,12 @@
 class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
+  @@genres = []
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@genres << genre if !@@genres.include?(genre)
     @@count += 1
   end
   def count
