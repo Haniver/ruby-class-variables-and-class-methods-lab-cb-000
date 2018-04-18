4f6a08dc0cf248a4393adc6882a9804521b53a40
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-04-18 19:53:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,10 +6,23 @@ class Song
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
-    @@artists << artist if !@@artists.include?(artist)
+    @@artists << artist
     @genre = genre
-    @@genres << genre if !@@genres.include?(genre)
+    @@genres << genre
     @@count += 1
+  end
+  def artists
+    unique_artists = []
+    @@artists.each do |artist|
+      unique_artists << artist if !unique_artists.include?(artist)
+    end
+    unique_artists
+  end
+  def genres
+    unique_genres = []
+    @@genres.each do |genre|
+      unique_genres << genre if !unique_genres.include?(genre)
+    end
   end
   def count
     @@count
